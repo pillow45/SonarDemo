dfdc4ef71c5c2d081a631eb317f85826429222a9
Your Name <you@example.com>
2021-05-21 18:06:30 +0900

    added sample codes

---
+++ src/test/java/com/example/sonardemo/SonardemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonardemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonardemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
